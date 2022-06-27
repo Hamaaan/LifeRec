d6f9b12c75c08392fa1138ab9b521108a0a836cd
Hamaaan <51164087+Hamaaan@users.noreply.github.com>
2022-06-28 06:17:51 +0900

    k

---
+++ Assets/Hamaaan/Scripts/TouchGraphManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TouchGraphManager : MonoBehaviour, IDragHandler, IDropHandler, IPointerEnterHandler
+{
+    [SerializeField] Color[] colors;
+
+    Image rend;
+
+    private void Start()
+    {
+        rend = GetComponent<Image>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (Input.GetMouseButton(0))
+        {
+            rend.color = colors[0];
+        }
+
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rend.color = colors[0];
+
+        //Debug.Log("drag");
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        //Destroy(gameObject);
+        //Debug.Log("drop");
+
+    }
+
+}
