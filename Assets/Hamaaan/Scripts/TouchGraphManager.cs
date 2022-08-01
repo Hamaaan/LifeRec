72998b1feeb9ffc9eae5611362856fa4921e9bfa
Hamaaan <51164087+Hamaaan@users.noreply.github.com>
2022-08-01 12:38:32 +0900

    k

--- Assets/Hamaaan/Scripts/TouchGraphManager.cs
+++ Assets/Hamaaan/Scripts/TouchGraphManager.cs
@@ -3,12 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using System; //DateTimeを使用する為追加。
 
 public class TouchGraphManager : MonoBehaviour, IDragHandler, IDropHandler, IPointerEnterHandler
 {
     public Color color;
 
+    public Color moodColor;
+    public Color actionColor;
+    public Color conditionColor;
+    public Color stickerColor;
+
     Image rend;
+
+    [SerializeField] Dropdown dropdown1;
+    [SerializeField] Dropdown dropdown2;
+    [SerializeField] Dropdown dropdown3;
+    [SerializeField] Dropdown dropdown4;
 
     [SerializeField] Image PickedColor;
 
@@ -16,7 +27,16 @@ public class TouchGraphManager : MonoBehaviour, IDragHandler, IDropHandler, IPoi
     {
         rend = GetComponent<Image>();
         PickedColor = PickedColor.GetComponent<Image>();
-        
+
+        dropdown1 = dropdown1.GetComponent<Dropdown>();
+        dropdown2 = dropdown2.GetComponent<Dropdown>();
+        dropdown3 = dropdown3.GetComponent<Dropdown>();
+        dropdown4 = dropdown4.GetComponent<Dropdown>();
+
+        //ブロックの番地を名前に割り当て
+        transform.name = transform.parent.GetSiblingIndex().ToString() + transform.GetSiblingIndex().ToString("D2");
+
+        blockDataSave();
     }
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -27,7 +47,6 @@ public class TouchGraphManager : MonoBehaviour, IDragHandler, IDropHandler, IPoi
         {
             rend.color = color;
         }
-
 
     }
 
@@ -41,8 +60,39 @@ public class TouchGraphManager : MonoBehaviour, IDragHandler, IDropHandler, IPoi
     public void OnDrop(PointerEventData eventData)
     {
         //Destroy(gameObject);
-        //Debug.Log("drop");
 
+        //Debug.Log("drop");
     }
 
+    //ブロックデータのロード/セーブ
+
+    public string keyname = null;
+    public string blockData = null;
+
+    public void blockDataSave()
+    {
+        DateTime timestamp = DateTime.Now;
+
+        keyname = timestamp.Year.ToString() + timestamp.Month.ToString("D2") + timestamp.Day.ToString("D2") + this.gameObject.name;
+
+
+
+        dropdown1.captionText.text = "a";
+
+
+        blockData = "02,01,03,00";
+
+        Debug.Log(keyname + " : " + blockData);
+
+        //Debug.Log(timestamp.TimeOfDay.ToString().Substring(0,5));
+    }
+
+    void OnDestroy()
+    {
+        // blockData_(番地)　というキーでblockDataを格納
+        PlayerPrefs.SetString("blockData_" + this.gameObject.name, keyname);
+        PlayerPrefs.Save();
+    }
+
+    
 }
