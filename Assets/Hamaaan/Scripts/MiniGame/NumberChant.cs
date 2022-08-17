84e510c2d3746868b6d24fee238652d5e2458662
Hamaaan <51164087+Hamaaan@users.noreply.github.com>
2022-08-17 19:16:08 +0900

    a

---
+++ Assets/Hamaaan/Scripts/MiniGame/NumberChant.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class NumberChant : MonoBehaviour
+{
+
+    [SerializeField] int NumberRow;
+
+    [SerializeField] Text text;
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        string NumbersRow;
+
+        NumbersRow = ((int)Random.Range(1, 9)).ToString() + ((int)Random.Range(1, 9)).ToString() + ((int)Random.Range(1, 9)).ToString() + ((int)Random.Range(1, 9)).ToString();
+
+
+
+
+
+
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+}
