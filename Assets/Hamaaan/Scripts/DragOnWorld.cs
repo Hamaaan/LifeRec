ce6be22829fe815ee8074bde326e35b1b990cb52
Hamaaan <51164087+Hamaaan@users.noreply.github.com>
2022-09-03 21:41:00 +0900

    f

---
+++ Assets/Hamaaan/Scripts/DragOnWorld.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class DragOnWorld : MonoBehaviour, IDragHandler, IDropHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        Rigidbody2D rd = GetComponent<Rigidbody2D>();
+
+        if (GetComponent<Rigidbody2D>())
+        {
+            rd.isKinematic = true;
+        }
+
+        Vector3 TargetPos = Camera.main.ScreenToWorldPoint(eventData.position);
+        TargetPos.z = 0;
+        transform.position = TargetPos;
+
+        if (GetComponent<Rigidbody2D>())
+        {
+            rd.isKinematic = false;
+        }
+
+        //Debug.Log("drag");
+
+
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        //Destroy(gameObject);
+        //Debug.Log("drop");
+
+    }
+}
---
+++ Assets/Hamaaan/Scripts/MiniGame/BalanceManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalanceManager : MonoBehaviour
+{
+    [SerializeField] HingeJoint2D joint;
+
+    [SerializeField] bool isBalanced = false;
+
+    JointAngleLimits2D defaultLimits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        joint = joint.GetComponent<HingeJoint2D>();
+        defaultLimits = joint.limits;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        JointAngleLimits2D BalanceLimits = new JointAngleLimits2D();
+        BalanceLimits.max = 0;
+        BalanceLimits.min = 0;
+
+        if (isBalanced)
+        {
+            
+            joint.limits = BalanceLimits;
+        }
+        else
+        {
+            joint.limits = defaultLimits;
+        }
+    }
+}
