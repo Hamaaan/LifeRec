55f98f2f9b40530a18d8ef320c0bf1f45df1c1fd
Hamaaan <51164087+Hamaaan@users.noreply.github.com>
2022-10-19 19:51:57 +0900

    k

---
+++ Assets/HouchiDungeon/StaticResources/Scripts/Player/StatusDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusDisplay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //StaticValueManager.instance.subroutine();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Text>().text = "PlayerHP : " + StaticValueManager.instance.PlayerHP.ToString();
+
+
+    }
+}
---
+++ Assets/HouchiDungeon/StaticResources/Scripts/TimerStartManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class TimerStartManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SceneChangeTo(int num)
+    {
+        SceneManager.LoadScene(num);
+    }
+
+    public void SceneChangeTo(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
