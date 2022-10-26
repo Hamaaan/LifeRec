cfe2d0ca4803723cf586ab629f2c52094179a4ae
wonokokko <115980348+wonokokko@users.noreply.github.com>
2022-10-27 07:14:42 +0900

    minigame_puzzle
    あとちょっと

---
+++ Assets/Ono/IQPuzzle/scripts/ClickToRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickToRotation : MonoBehaviour
+{
+     [SerializeField]
+    [Tooltip("NextRotation")]
+    private GameObject NextRotation;
+
+    public void onClickAct()
+     {  
+        Vector3 tmp = this.gameObject.transform.position;
+        Instantiate(NextRotation, new Vector3(tmp.x, tmp.y, tmp.z), Quaternion.identity);
+        Destroy( this.gameObject );
+        //Debug.Log("タッチされたにゃー");
+
+    }
+}
+
