49dee4e18b35e812322777efba9e9d0f8582a78c
SotSpepo <yuuki.072092@gmail.com>
2022-10-28 05:02:52 +0900

    UIManager

---
+++ Assets/HouchiDungeon/StaticResources/Scripts/UI/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    StaticValueManager _svm;
+
+    public Text LevelText;
+    public Text ExpText;
+    //public Text PlayerText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _svm = StaticValueManager.instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LevelText.text = _svm.PlayerLevel.ToString();
+        ExpText.text = _svm.PlayerExp.ToString();
+    }
+}
