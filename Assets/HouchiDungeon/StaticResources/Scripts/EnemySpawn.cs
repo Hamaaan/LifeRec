e6c26a19ad6a3d2404b5e15e9ec0400caf986345
Hamaaan <51164087+Hamaaan@users.noreply.github.com>
2022-10-30 10:37:16 +0900

    r

--- Assets/HouchiDungeon/StaticResources/Scripts/EnemySpawn.cs
+++ Assets/HouchiDungeon/StaticResources/Scripts/EnemySpawn.cs
@@ -5,23 +5,45 @@ using UnityEngine;
 public class EnemySpawn : MonoBehaviour
 {
     [SerializeField] GameObject[] EnemyPrefabs;
-
     public int enemyCount;
+
+    SpawnColliderManager spm;
 
     // Start is called before the first frame update
     void Start()
     {
+        spm = GetComponent<SpawnColliderManager>();
         GenerateEnemys();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (spm.isSpawn)
+        {
+            GenerateEnemys();
+        }
     }
 
     public void GenerateEnemys()
     {
+        GameObject enemy = Instantiate(EnemyPrefabs[Random.Range(0, (EnemyPrefabs.Length-1))]);
+
+        float r = Random.Range(0, 1f);
+        float g = Random.Range(0, 1 - r);
+        float b = 1 - r - g;
+        //enemy.GetComponent<SpriteRenderer>().material.color = new Color(r, g, b, 1) * 2f;
+
+        enemy.transform.SetParent(this.gameObject.transform);
+        enemy.transform.position += new Vector3(spm.col.transform.position.x - (Random.Range(0.4f, 2f)), 0, 1);
+        if (enemy.name == "BirdEnemy(Clone)")
+        {
+            enemy.transform.position += new Vector3(0, Random.Range(0.2f, 3f), 1);
+        }
+
+
+        /*
         for (int i = 0; i < enemyCount; i++)
         {
             GameObject enemy = Instantiate(EnemyPrefabs[Random.Range(0, (EnemyPrefabs.Length))]);
@@ -38,5 +60,6 @@ public class EnemySpawn : MonoBehaviour
                 enemy.transform.position += new Vector3(0, Random.Range(2, 3), 1);
             }
         }
+        */
     }
 }
